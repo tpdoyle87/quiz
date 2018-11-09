d1ee4051d354d5b40c876b399f05911056e28b2c
Thomas DoyleEngler <tpdoyle87@gmail.com>
2018-11-08 23:22:55 -0500

    Fonts and JSON

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,15 +1,24 @@
 $("#my-form").submit(function(event) {
   event.preventDefault();
-  var form_data = $(this).serialize();
-  var post_url = $(this).attr("action");
-  var request_method = $(this).attr("method");
-  console.log(form_data)
+  let form_data = $(this).serializeArray();
+  const data = {}
+  $(form_data).each(function(index, obj){
+    data[obj.name] = obj.value;
+  })
+  const post_url = $(this).attr("action");
+  const request_method = $(this).attr("method");
+  console.log(data)
   $.ajax({
     url : post_url,
     type: request_method,
-    data : form_data
+    data : data
   }).done(function(response){ //
     $("#server-results").html(response);
   });
 })
 
+var formdata = $("#myform").serializeArray();
+var data = {};
+$(formdata ).each(function(index, obj){
+    data[obj.name] = obj.value;
+});
